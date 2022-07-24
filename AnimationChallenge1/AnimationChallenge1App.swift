763579b792671568ce37e99b39d0c05ff4a88653
Josue Hernandez <jghg.02@gmail.com>
2022-07-24 19:42:11 -0400

    Animation Challenge

---
+++ AnimationChallenge1/AnimationChallenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationChallenge1App.swift
+//  AnimationChallenge1
+//
+//  Created by Josue Hernandez on 13-07-22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationChallenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            BarAnimationScreen()
+        }
+    }
+}
